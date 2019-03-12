6db02eb279630f905d842e2995bb4312ae96f000
Mohammad Mohammadi <mohammad@exabyte.io>
2019-03-12 16:28:06 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='exabyte-api-client',
     description='Exabyte Python Client for RESTful API',
-    version='2018.10.01',
+    version='1.0.0',
     url='http://github.com/Exabyte-io/exabyte-api-client',
-    author='Mohammad Mohammadi',
-    author_email='mohammad@exabyte.io',
-    license='Exabyte Inc.',
+    author='Exabyte Inc.',
+    author_email='info@exabyte.io',
     packages=find_packages(exclude=['tests*']),
     install_requires=[
         'mock==1.3.0',
-        'requests==2.20.1',
-
+        'requests==2.20.1'
     ],
     classifiers=[
         'Programming Language :: Python',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Software Development'
+        'Topic :: Software Development',
+        'License :: OSI Approved :: Apache Software License'
     ],
 )
